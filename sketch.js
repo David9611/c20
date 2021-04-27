012c1ab2ceb35cff25779f83fbd4608a332d5ca9
David9611 <79354522+David9611@users.noreply.github.com>
2021-04-27 19:42:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,69 @@
-function setup() {
-  createCanvas(800,400);
- fixedRect = createSprite(200, 200, 50, 80);
-movingRect = createSprite(400, 200, 80, 30);
-movingRect.shapeColor = "green"
-fixedRect.shapeColor = "green"
+var bgImg
+var cat, catimg1, catimg2;
+var mouse, mouseimg1, mouseimg2;
+
+function preload() {
+    //load the images here
+bgImg = loadImage("garden.png")
+catimg1 = loadAnimation("images/cat1.png")
+catimg2 = loadAnimation("images/cat2.png", "images/cat3.png")
+catimg3 = loadAnimation("images/cat4.png")
+mouseimg1 = loadAnimation("images/mouse1.png")
+mouseimg2 = loadAnimation("images/mouse2.png","images/mouse3.png")
+mouseimg3 = loadAnimation("images/mouse4.png")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+cat = createSprite(870, 600)
+cat.addAnimation("catSleeping", "catimg1")
+cat.scale = 0.2
+
+mouse = createSprite(200,600)
+mouse.addAnimation("mouseSleeping", "mouseimg1")
+mouse.scale = 0.15
 
 }
 
 function draw() {
-  background(255,255,255);  
- movingRect.x = World.mouseX
- movingRect.y = World.mouseY
- if(movingRect.x - fixedRect.x < fixedRect.width/2+movingRect.width/2
-  && fixedRect.x - movingRect.x < fixedRect.width/2+movingRect.width/2
-  && movingRect.y - fixedRect.y < fixedRect.height/2+movingRect.height/2
-  && fixedRect.y - movingRect.y < fixedRect.height/2+movingRect.height/2){
-  movingRect.shapeColor = "red"
-  fixedRect.shapeColor = "red"
- }
- else{
-  movingRect.shapeColor = "green"
-  fixedRect.shapeColor = "green"
- }
-  drawSprites();
+background(bgImg)
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+if(cat.x - mouse.x < (cat.width - mouse.width)/2){
+
+cat.velocityX = 0
+cat.addAnimation("catFinalImg", catimg3)
+cat.x = 300
+cat.scale = 0.2
+cat.changeAnimation("catFinalImg")
+
+mouse.addAnimation("mouseFinalImg", mouseimg3)
+mouse.scale = 0.15
+mouse.changeAnimation("mouseFinalImg")
+
+}
+    drawSprites();
+}
+
+
+function keyPressed(){
+  //For moving and changing animation write code here
+
+if(keyCode === LEFT_ARROW){
+cat.velocityX = -5
+cat.addAnimation("catRunning", catimg2 )
+cat.changeAnimation("catRunning")
+
+}
+
+if(keyCode === RIGHT_ARROW){
+mouse.addAnimation("mouseRunning", mouseimg2)
+mouse.frameDelay = 25;
+mouse.changeAnimation("mouseRunning")
+
+}
+
 }
